3f308c718962119fd503354b177a83e93df9626c
Neha <103049412+NehaTiwari2706@users.noreply.github.com>
2023-01-01 21:59:59 +0530

    Update Mq2.cpp

--- Microsoft/Mq2.cpp
+++ Microsoft/Mq2.cpp
@@ -1,4 +1,23 @@
-// Rotate Function
+/*  Rotate Function 
+
+original array[] = {4,3,2,6}
+F1(original array) = 4*0 + 3*1 + 2*2 + 6*3
+
+upon 1 rotation = {6,4,3,2}
+F2 = 6*0 + 4*1 + 3*2 + 2*3
+
+What do we observe between F1 AND F2
+
+i) We observe that we multiply each element of original array (except the last one) with one more than the previous time.
+Earlier   4*0 + 3*1 + 2*2 + 6*3
+Now       6*0 + 4*1 + 3*2 + 2*3
+
+How we get from earlier to now 
+     ( 4*0 + 3*1 + 2*2 + 3*6 ) + (4+3+2+6) - 6*4 = now
+     now = earlier + sum - (last element * n)
+     where n = size of array here n = 4
+           sum = sum of original array elements. 
+*/
 
 class Solution {
 public:
