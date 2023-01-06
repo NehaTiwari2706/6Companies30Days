82758cc148dde50b276576988f48eab549d5f7eb
Neha <103049412+NehaTiwari2706@users.noreply.github.com>
2023-01-06 22:12:22 +0530

    Create Gq1.cpp

---
+++ Goldman Sachs/Gq1.cpp
@@ -0,0 +1,25 @@
+// FACTORIAL WITH TRAILING ZEROES
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+           
+        if(n < 0){
+            return -1;
+        }
+        
+        int count = 0;
+        
+        for(int i=5; n/i >= 1; i*=5){
+            
+            count += n/i;
+        }
+        
+        return count;
+    }
+};
+
+/*
+Time Complexity = O(logn)
+Space Complexity = O(1)
+*/
